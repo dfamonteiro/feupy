d480e592ad08329ad7cc68edf3da002aa87556b9
Daniel Monteiro <up201806185@fe.up.pt>
2019-10-02 23:34:51 +0100

    version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = '2019, Daniel Monteiro'
 author = 'Daniel Monteiro'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.1'
+release = '0.3.2'
 
 master_doc = 'index'
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="feupy",
-    version="0.3.1",
+    version="0.3.2",
     author="Daniel Monteiro",
     author_email="up201806185@fe.up.pt",
     description="The sigarra scraping library no one asked for",
