582ea693bfa1ede0be9678f6a7f7423a62251368
Daniel Monteiro <up201806185@fe.up.pt>
2019-10-03 00:05:55 +0100

    Fixed botched release and version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = '2019, Daniel Monteiro'
 author = 'Daniel Monteiro'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.2'
+release = '0.3.4'
 
 master_doc = 'index'
 # -- General configuration ---------------------------------------------------
