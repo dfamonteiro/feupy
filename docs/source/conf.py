301f78fba5fea16d75bf429bdf52d9b64c6bf8f2
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2020-07-30 13:59:38 +0100

    Version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = '2019, Daniel Monteiro'
 author = 'Daniel Monteiro'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.7'
+release = '0.4.8'
 
 master_doc = 'index'
 # -- General configuration ---------------------------------------------------
